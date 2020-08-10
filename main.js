b1dfa3d3f84c76d8f1f36e6fdbdd13c5e6fa5b90
surendraradaye <surendra.radaye@centurylink.com>
2020-08-10 23:49:30 +0000

    Lab 1

--- main.js
+++ main.js
@@ -11,19 +11,12 @@ const IPCIDR = require('ip-cidr');
  * @param {string} cidrStr - The IPv4 subnet expressed
  *                 in CIDR format.
  * @param {callback} callback - A callback function.
- * @return {object} (firstIpAddress) - An object with 2 properties - ipv4 and ipv6 whose values are string
+ * @return {string} (firstIpAddress) - An IPv4 address.
  */
-
 function getFirstIpAddress(cidrStr, callback) {
 
   // Initialize return arguments for callback
-
-  let firstIpAddress = {
-	  ipv4: null,
-	  ipv6: null
-  };
-
-  //let firstIpAddress = null;
+  let firstIpAddress = null;
   let callbackError = null;
 
   // Instantiate an object from the imported class and assign the instance to variable cidr.
@@ -43,16 +36,7 @@ function getFirstIpAddress(cidrStr, callback) {
   } else {
     // If the passed CIDR is valid, call the object's toArray() method.
     // Notice the destructering assignment syntax to get the value of the first array's element.
-    //[firstIpAddress] = cidr.toArray(options);
-    console.log(`\n--- Inside getFirstIpAddress(${cidr.toArray(options)}) ---`);
-    //let ipv4First = cidr.toArray(options).toString();
-    //firstIpAddress.ipv4 = ipv4First;
-    //let mappedAddress = getIpv4MappedIpv6Address(ipv4First);
-    //firstIpAddress.ipv6 = mappedAddress;
-    //[firstIpAddress] = [ipv4First,mappedAddress];
-    [firstIpAddress.ipv4] = cidr.toArray(options);
-    firstIpAddress.ipv6 = getIpv4MappedIpv6Address(firstIpAddress.ipv4);
-    
+    [firstIpAddress] = cidr.toArray(options);
   }
   // Call the passed callback function.
   // Node.js convention is to pass error data as the first argument to a callback.
@@ -61,21 +45,18 @@ function getFirstIpAddress(cidrStr, callback) {
   return callback(firstIpAddress, callbackError);
 }
 
+
 /*
   This section is used to test function and log any errors.
   We will make several positive and negative tests.
 */
 function main() {
-  // Create some test data for getFirstIpAddress(), both valid and invalid.
+  // Create an array of tests with both valid CIDR and invalid IP subnets.
   let sampleCidrs = ['172.16.10.0/24', '172.16.10.0 255.255.255.0', '172.16.10.128/25', '192.168.1.216/30'];
   let sampleCidrsLen = sampleCidrs.length;
-  // Create some test data for getIpv4MappedIpv6Address, both valid and invalid.
-  let sampleIpv4s = [ '172.16.10.1', '172.16.10.0/24', '172.16.10.0 255.255.255.0', '172.16.256.1', '1.1.1.-1'];
-  let sampleIpv4sLen = sampleIpv4s.length;
 
   // Iterate over sampleCidrs and pass the element's value to getFirstIpAddress().
-  
-  for (let i = 0; i < sampleCidrsLen; i++) {
+  for (var i = 0; i < sampleCidrsLen; i++) {
     console.log(`\n--- Test Number ${i + 1} getFirstIpAddress(${sampleCidrs[i]}) ---`);
     // Call getFirstIpAddress and pass the test subnet and an anonymous callback function.
     // The callback is using the fat arrow operator: () => { }
@@ -83,74 +64,13 @@ function main() {
       // Now we are inside the callback function.
       // Display the results on the console.
       if (error) {
-        console.error(`  Error returned from GET request: ${error}`);
+        console.error(`Error returned from GET request: ${error}`);
+      } else {
+        console.log(`Response returned from GET request: ${data}`);
       }
-      console.log(`  Response returned from GET request: ${data.ipv6}`);
     });
   }
-  
-  // Iterate over sampleIpv4s and pass the element's value to getIpv4MappedIpv6Address().
-  for (let i = 0; i < sampleIpv4sLen; i++) {
-    console.log(`\n--- Test Number ${i + 1} getIpv4MappedIpv6Address(${sampleIpv4s[i]}) ---`);
-    // Assign the function results to a variable so we can check if a string or null was returned.
-    let mappedAddress = getIpv4MappedIpv6Address(sampleIpv4s[i]);
-    if( mappedAddress ) {
-      console.log(`  IPv4 ${sampleIpv4s[i]} mapped to IPv6 Address: ${mappedAddress}`);
-    } else {
-      console.error(`  Problem converting IPv4 ${sampleIpv4s[i]} into a mapped IPv6 address.`);
-    }
-  }
 }
-
-/**
- * Calculates an IPv4-mapped IPv6 address.
- * @param {string} ipv4 - An IPv4 address in dotted-quad format.
- * @return {*} (ipv6Address) - An IPv6 address string or null if a run-time problem was detected.
- */
-function getIpv4MappedIpv6Address(ipv4) {
-
-  // Initialize return argument
-  let ipv6Address = null;
-
-  // Prepare to derive a Hex version of the dotted-quad decimal IPv4 address.
-  // Split the IPv4 address into its four parts.
-  let ipv4Quads = ipv4.split('.');
-  // Count the number of parts found.
-  let numIpv4Segments = ipv4Quads.length;
-
-  // Verify IPv4 had four parts.
-  if (numIpv4Segments === 4) {
-    let validQuads = true;
-    // Iterate over the IPv4 address parts and verify each segment was a number between 0 and 255.
-    for(let i=0; i < numIpv4Segments; i++) {
-      if( isNaN(Number(ipv4Quads[i])) || ipv4Quads[i] < 0 || ipv4Quads[i] > 255 ) {
-        validQuads = false;
-      }
-    }
-    // Passed IPv4 is valid. Now to derive an IPv4-mapped IPv6 address.
-    if (validQuads) {
-      // Hardcode the prefix. During refactor, we might want to make the prefix a const.
-      ipv6Address = "0:0:0:0:0:ffff:";
-      // Iterate over the IPv4 parts
-      for(let i=0; i < numIpv4Segments; i++) {
-        // Convert part to an integer, then convert to a hex string using method toString()
-        // with a base 16 (hex) encoding.
-        let hexString = parseInt(ipv4Quads[i]).toString(16);
-        // If hex is odd (single digit), prepend a '0'. This is why we wanted to work with a string.
-        if (hexString.length % 2)
-          hexString = '0' + hexString;
-        // Append hex part to evolving variable ipv6Address.
-        ipv6Address = ipv6Address + hexString;
-        // Add a colon to split the encoded address and match the IPv6 format.
-        if(i===1) {
-          ipv6Address = ipv6Address + ':';
-        }
-      }
-    }
-  }
-  return ipv6Address;
-}
-
 
 /*
   Call main to run it.
